6c5ed3a4a97a82b0b3faba3903d1c856e9156f18
athornton95 <athornton.co@gmail.com>
2021-10-21 12:39:56 -0600

    misspelling

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -12,7 +12,7 @@ const Events = () => (
       <p className='description'>From weddings, birthdays, corporate gifts, or any celebration -- pie will bring smiles and is right for any occasion!</p>
       <em className='additional'>*Custom options available, delivery available*</em>
       <div className='contact-block'>
-        <p>Interested in working together? Shoot us an email or give us a call at the bakery for a consulation!</p>
+        <p>Interested in working together? Shoot us an email or give us a call at the bakery for a consultation!</p>
         <a className='btn btn-cta' href = "mailto:elias@coloradocherrycompany.com?subject=Cherry Co Info Request">Let's Chat Pie</a>
         <div className='call-block'>
           <p>or give our bakery a call</p>
