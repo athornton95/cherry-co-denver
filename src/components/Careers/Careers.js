32b60eb338e30ca6da6d7f671856487957219b27
athornton95 <athornton.co@gmail.com>
2021-10-21 12:45:34 -0600

    add caps to cta

--- src/components/Careers/Careers.js
+++ src/components/Careers/Careers.js
@@ -8,7 +8,7 @@ const Careers = () => {
       <Container maxWidth='md'>
         <div className='content'>
           <p className='text'>We're always looking for awesome folks to join our pie family. Interested?</p>
-          <a className='btn btn-cta' href="mailto:elias@coloradocherrycompany.com?subject=I want to join the pie fam!">Send us your resume</a>
+          <a className='btn btn-cta' href="mailto:elias@coloradocherrycompany.com?subject=I want to join the pie fam!">Send Us Your Resume</a>
         </div>
       </Container>
     </div>
